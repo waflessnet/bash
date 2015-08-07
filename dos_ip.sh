55c479a1e9c0657ad552623297d0f6cc65bc7c53
root <root@apache-smdeck>
2015-08-07 15:38:09 -0300

    correcciones minimas

--- dos_ip.sh
+++ dos_ip.sh
@@ -1,8 +1,13 @@
-#i!/bin/bash
+#!/bin/bash
+# Autor: francisco@twpanel.com
+# 
+# Este script permite levantar dos ip diferente en dos tarjetas de red.
+# se asume  que las tarjetas levantan en eth0 y eth1
+# se asume que publica es una red e interna es otra red.
 TABLA_1="t0"
 TABLA_2="t1"
-IP_PUBLICA=$(hostname -I | cut -d' ' -f1)
-IP_INTERNA=$(hostname -I | cut -d' ' -f2)
+IP_PUBLICA=$(hostname -I | cut -d' ' -f1) #eth0
+IP_INTERNA=$(hostname -I | cut -d' ' -f2) #eth1 
 echo "Leyendo ..."
 if [ -z "$(cat /etc/iproute2/rt_tables | grep -w $TABLA_1)" ]; then
 	echo "creando Tabla $TABLA_1 ..."
@@ -28,14 +33,14 @@ echo "Gateway ip publica: "$GATEWAY_IP_PUBLICA
 echo "Gateway ip interna: "$GATEWAY_IP_INTERNA
 
 #seteamos los gateway por default
-
+# gw xxxx.xxxx.xxxx.1 
 route add default gw $GATEWAY_IP_PUBLICA eth0
 route add default gw $GATEWAY_IP_INTERNA eth1
 
-#Aplicamos route por defualt  t1
+#Aplicamos route por defualt  t10 y t1
 ip route add default via $GATEWAY_IP_PUBLICA  dev eth0 table t0
 ip route add default via $GATEWAY_IP_INTERNA  dev eth1 table t1
-#segmentos
+
 #ip route add  $RED_INTERNA/24 via $IP_INTERNA
 #ip route add  $RED_PUBLICA/24 via $IP_PUBLICA
 ip route add $RED_INTERNA/24 dev eth1 src $IP_INTERNA table t1
@@ -47,28 +52,15 @@ ip route add $RED_PUBLICA/24 dev eth0 src $IP_PUBLICA table t0
 # ip rule  | grep -w $TABLA_2
 # ...
 
-
 # Aplicamos reglas 
 ip rule add from $IP_PUBLICA table t0
 ip rule add from $IP_INTERNA table t1
 #aplicamos fordward 
-echo "1" > /proc/sys/net/ipv4/ip_forward
+#echo "1" > /proc/sys/net/ipv4/ip_forward
 
 # comenzamos con ip publica """la lanzamos siempre
 # ip route add default via $GATEWAY  dev eth0 table t2
 
 
 # Al finalizar
-#ip route flush cache
-
-
-
-
-
-
-
-
-
-
-
-
+ip route flush cache
